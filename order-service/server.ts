48a907ba170e6fb0d31402c3e4bd0dc0e33db8b4
arok201001 <164240289+arok201001@users.noreply.github.com>
2026-06-17 01:33:27 +0200

    initial typescript setup for product and order services

---
+++ order-service/server.ts
@@ -0,0 +1,83 @@
+import fastify from 'fastify';
+import { Pool } from 'pg';
+import amqp, { Channel } from 'amqplib';
+import nodeHttp2 = require('node:http2');
+
+const server = fastify({ logger: true });
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3002;
+const DATABASE_URL = process.env.DATABASE_URL;
+const RABBITMQ_URL = process.env.RABBITMQ_URL || 'amqp://guest:guest@rabbitmq:5672';
+
+const pool = new Pool({
+    connectionString: DATABASE_URL,
+
+}),
+
+let channel: Channel;
+
+async function connectRabbit() {
+    try {
+        const connection = await amqp.connect(RABBITMQ_URL);
+        channel = await connection.createChannel();
+        await channel.assertQueue("order_queue", { durable: true });
+        server.log.info("Order Service ansluten till RabbitMQ!");
+    } catch (err) {
+        server.log.error("Kunde inte ansluta till RabbitMQ, försöker igen om 5 sekunder...", err);
+        setTimeout(connectionRabbit, 5000);
+    }
+}
+
+connectRabbit();
+
+interface OrderItem {
+    id: number;
+    name: string;
+    quantity: number;
+}
+
+interface OrderRequest {
+    customerId: string;
+    items: OrderItem[];
+}
+
+server.post("/api/orders", async (request, reply) => {
+    const { customerId, items } = request.body as OrderRequest;
+
+    if (!customerId || !items) {
+        return reply.status(400).send({ error: "customerId och items krävs." });
+
+    }
+
+    const order = {
+        id: Math.floor(Math.random() * 10000),
+        customerId,
+        items,
+        status: "PENDING",
+        createdAt: new Date()
+    };
+
+    try {
+        if (channel) {
+            channel.sendToQueue("order_queue", Buffer.from(JSON.stringify(order)), { persistent: true });
+            return reply.status(202).send({
+                message: "Order mottagen!",
+                orderId: order.id
+            });
+        } else {
+            return reply.status(503).send({ error: "Kötjänsten är inte redo än." });
+        }
+    } catch (error) {
+        server.log.error(error);
+        return reply.status(500).send({ error: "Internt systemfel." });
+    }
+});
+
+const start = async () => {
+    try {
+        await server.listen({ port: PORT, host: "0.0.0.0" });
+    } catch (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
+};
+start();
---
+++ product-service/server.ts
@@ -0,0 +1,31 @@
+import fastify from "fastify";
+
+const server = fastify({ logger: true });
+const PORT = process.env.PORT ? parseInt(process.env.port) : 3001;
+
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+}
+
+const menu: Product[] = [
+    { id: 1, name: "Falafelburgare", price: 89 },
+    { id: 2, name: "Pommes", price: 35 },
+    { id: 3, name: "Islatte", price: 29 },
+    { id: 4, name: "Läsk", price: 25 }
+];
+
+server.get("/api/products", async (request, reply) => {
+    return menu;
+});
+
+const start = async () => {
+    try {
+        await server.listen({ port: PORT, host: "0.0.0.0" });
+    } catch (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
+};
+start();
