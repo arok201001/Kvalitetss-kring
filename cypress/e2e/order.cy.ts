b78877597b7a9240337f7da5f3f3039e933fd0e2
arok201001 <164240289+arok201001@users.noreply.github.com>
2026-06-17 13:32:34 +0200

    cypress update

--- cypress/e2e/order.cy.ts
+++ cypress/e2e/order.cy.ts
@@ -1,5 +1,17 @@
-describe('Fastfood System E2E', () => {
-  it('Ska skicka order via gateway och fa 202', () => {
+describe('Fastfood System E2E & Integrationstester', () => {
+
+  it('Ska kunna hämta alla produkter och menyer från product-service', () => {
+    cy.request({
+      method: 'GET',
+      url: '/api/products'
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.an('array');
+      expect(response.body.length).to.be.greaterThan(0);
+    });
+  });
+
+  it('Ska skicka en giltig order och få 202 Accepted', () => {
     cy.request({
       method: 'POST',
       url: '/api/orders',
@@ -15,4 +27,29 @@ describe('Fastfood System E2E', () => {
       expect(response.body.message).to.eq('Order mottagen!');
     });
   });
+
+  it('Ska neka ordern med 400 om items-listan är tom', () => {
+    cy.request({
+      method: 'POST',
+      url: '/api/orders',
+      failOnStatusCode: false,
+      body: {
+        customerId: "Arkan_Fel_Test",
+        items: []
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+    });
+  });
+
+  it('Ska ge 404 om man försöker hämta en order som inte existerar', () => {
+    cy.request({
+      method: 'GET',
+      url: '/api/orders/finns-inte-i-databasen-123',
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(404);
+    });
+  });
+
 });
